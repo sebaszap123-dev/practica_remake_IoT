9c72fa73d31ea0416f64119bc789d7b60c6bea5b
sebaszap123-dev <freeway.dev.sf@gmail.com>
2022-09-28 21:31:44 -0500

    comment code

--- practica_remake/practica_remake.ino
+++ practica_remake/practica_remake.ino
@@ -1,9 +1,13 @@
 #include "serial_functions.h"
+// Create instant of class
 serial_class calcular;
-void setup() {
+void setup()
+{
   Serial.begin(115200);
 }
 
-void loop() {
+void loop()
+{
+  // Call function of instance serial_class for get incomming data from serial monitor
   calcular.get_data();
 }
--- practica_remake/serial_functions.h
+++ practica_remake/serial_functions.h
@@ -2,8 +2,11 @@ class serial_class
 {
 
 public:
+  // Function can be called
   void get_data(void);
+
 private:
+  // Functions that only can be called inside the class
   void calculateUnsigned(int);
   void calculateSigned(int);
   void isSigned(String, int);
@@ -16,35 +19,44 @@ private:
   void i16(int);
   void i8(int);
 };
-
+// Await for value sended in serial monitor
 void serial_class::get_data(void)
 {
-  if (Serial.available() > 0) {
-    String command = Serial.readStringUntil('\n'); // read string until newline character
-    int index = command.indexOf(" ");
-    String number = command.substring(0, index);
-    String type = command.substring(index+1, command.length());
-    if(number != ""){
+  if (Serial.available() > 0)
+  {
+    String command = Serial.readStringUntil('\n');                // read string until newline character
+    int index = command.indexOf(" ");                             // get index of blank space
+    String number = command.substring(0, index);                  // get number
+    String type = command.substring(index + 1, command.length()); // get type string
+    if (number != "")
+    {
       int myInt = command.toInt();
-      if(myInt > 0){
+      if (myInt > 0)
+      {
         isSigned(type, myInt);
       }
     }
   }
 }
-
-void serial_class::isSigned(String type, int number){
- if (type == "unsigned"){
-  // Unsigned values
-  calculateUnsigned(number);
- }else {
-  // singend values
-  calculateSigned(number);
- }
+// check if is signed or unsigned number
+void serial_class::isSigned(String type, int number)
+{
+  if (type == "unsigned")
+  {
+    // Unsigned values
+    calculateUnsigned(number);
+  }
+  else
+  {
+    // singend values
+    calculateSigned(number);
+  }
 }
 
-void serial_class::calculateUnsigned(int number){
-  switch (number) {
+void serial_class::calculateUnsigned(int number)
+{
+  switch (number)
+  {
   case 8:
     u8(number);
     break;
@@ -60,11 +72,13 @@ void serial_class::calculateUnsigned(int number){
   default:
     Serial.print("No valid");
     break;
-  }  
+  }
 }
 
-void serial_class::calculateSigned(int number){
-  switch (number) {
+void serial_class::calculateSigned(int number)
+{
+  switch (number)
+  {
   case 8:
     i8(number);
     break;
@@ -80,14 +94,13 @@ void serial_class::calculateSigned(int number){
   default:
     Serial.print("No valid");
     break;
-  }  
+  }
 }
 
+// Make the maths for unsigned numbers
 
-
-
-
-void serial_class::u64(int number){
+void serial_class::u64(int number)
+{
   uint64_t test1;
   uint64_t test2;
   test1 = pow(2, number);
@@ -99,7 +112,8 @@ void serial_class::u64(int number){
   Serial.print("Min: ");
   Serial.println(test2);
 }
-void serial_class::u32(int number){
+void serial_class::u32(int number)
+{
   uint32_t test1;
   uint32_t test2;
   test1 = pow(2, number);
@@ -111,7 +125,8 @@ void serial_class::u32(int number){
   Serial.print("Min: ");
   Serial.println(test2);
 }
-void serial_class::u16(int number){
+void serial_class::u16(int number)
+{
   uint16_t test1;
   uint16_t test2;
   test1 = pow(2, number);
@@ -123,7 +138,8 @@ void serial_class::u16(int number){
   Serial.print("Min: ");
   Serial.println(test2);
 }
-void serial_class::u8(int number){
+void serial_class::u8(int number)
+{
   uint8_t test1;
   uint8_t test2;
   test1 = pow(2, number);
@@ -135,7 +151,11 @@ void serial_class::u8(int number){
   Serial.print("Min: ");
   Serial.println(test2);
 }
-void serial_class::i64(int number){
+
+// Make the maths for signed numbers
+
+void serial_class::i64(int number)
+{
   int64_t test1;
   int64_t test2;
   number -= 1;
@@ -147,7 +167,8 @@ void serial_class::i64(int number){
   Serial.print("Min: ");
   Serial.println(test2);
 }
-void serial_class::i32(int number){
+void serial_class::i32(int number)
+{
   int32_t test1;
   int32_t test2;
   number -= 1;
@@ -159,7 +180,8 @@ void serial_class::i32(int number){
   Serial.print("Min: ");
   Serial.println(test2);
 }
-void serial_class::i16(int number){
+void serial_class::i16(int number)
+{
   int16_t test1;
   int16_t test2;
   number -= 1;
@@ -171,7 +193,8 @@ void serial_class::i16(int number){
   Serial.print("Min: ");
   Serial.println(test2);
 }
-void serial_class::i8(int number){
+void serial_class::i8(int number)
+{
   int8_t test1;
   int8_t test2;
   number -= 1;
